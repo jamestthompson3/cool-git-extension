a2b296933c4be451c656c5af5e515998f37ccd93
Taylor Thompson <taylor.thompson@epicgames.com>
2019-10-10 17:57:10 +0300

    modifies regex

--- run.js
+++ run.js
@@ -1,10 +1,21 @@
 function walkText(node) {
-  console.log("walking text");
   if (node.nodeType == 3) {
-    node.data = node.data.replace(/pull\srequest/gi, "vibe check");
-    node.data = node.data.replace(/branch/gi, "mood");
-    node.data = node.data.replace(/merge\sconflict/gi, "bruh moment");
-    node.data = node.data.replace(/conflict/gi, "bruh moment");
+    const pr = /pull\srequest/gi;
+    const branch = /branch(es)?/gi;
+    const conflict = /merge?\sconflict/gi;
+    switch (true) {
+      case pr.test(node.data):
+        node.data = node.data.replace(pr, "vibe check");
+        break;
+      case branch.test(node.data):
+        node.data = node.data.replace(branch, "mood$1").replace("e", "");
+        break;
+      case conflict.test(node.data):
+        node.data = node.data.replace(conflict, "bruh moment");
+        break;
+      default:
+        break;
+    }
   }
   if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
     for (var i = 0; i < node.childNodes.length; i++) {
